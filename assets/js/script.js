ed51ff4c9885bb85e47734d431580b622fa711ca
scwebd <scwebd@gmail.com>
2019-05-09 13:25:54 -0600

    Adding initial code for review session app build

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// ======================
+// VARIABLES
+// ======================
+
+// 1st: pull initial budgetItems/lastId from localStorage to set initial values
+
+
+
+// ======================
+// FUNCTIONS
+// ======================
+
+// 4th: update localStorage with latest budgetItems and latest lastID
+
+
+// 5th: render budgetItems on table
+
+
+
+// ======================
+// MAIN PROCESS
+// ======================
+
+// 2nd: wire up click event on 'Enter New Budget Item' button to toggle display of form
+
+
+// 3rd: on click of 'Add Budget Item' button, gather user input and add newest item to budgetItems array
+// each budget item should include: id / date / name / category / amount / notes
+// (also, trigger localStorage update and trigger budgetItems rerender, once created)
+
+
+// 6th: on change of the category dropdown, show filtered budgetItems and total
+
+
+// 7th: on click of the delete button on a given row, delete that budgetItem
+
+
+
+
